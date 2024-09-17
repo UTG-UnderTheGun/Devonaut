7359ed392f05f4c1ff8113f1adb340d2a4d8093c
Tanakrit Maenphol <Thanagrith@gmail.com>
2024-09-18 01:00:24 +0700

    finished login frontend

--- client/src/app/auth/login/page.js
+++ client/src/app/auth/login/page.js
@@ -1,13 +1,57 @@
 import GlassBox from "@/components/glass-box";
+import './login.css'
 
 export default function Login() {
   return (
-    <div
-      className="bg-cover bg-center min-h-screen flex items-center justify-center"
-      style={{ backgroundImage: `url('https://res.cloudinary.com/dstl8qazf/image/upload/f_auto,q_auto/v1/underthegun/uy1kwt2wvxjwhq9sxa2x')` }}>
-      <GlassBox size={{ width: '27vw', height: '55vh' }}>
-        box test
+    <div className="bg-cover bg-center min-h-screen flex items-center justify-center"
+      style={{ backgroundImage: `url('/Login.png')` }}>
+      <GlassBox size={{ minWidth: '450px' }}>
+        <div className="login-form-container">
+          <div className="login-image-container">
+            <img
+              src="https://res.cloudinary.com/dstl8qazf/image/upload/f_auto,q_auto/v1/underthegun/hxvq5wvnp4xbxtnl7rrn"
+              alt="Profile"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="login-text">
+            <h1><strong>Log in</strong></h1>
+          </div>
+          <div className="login-form">
+            <form>
+              <div className="login-container">
+                <div className="login-username">
+                  <div>
+                    <label>Username</label>
+                  </div>
+                  <div>
+                    <input type="text" name="username" />
+                  </div>
+                </div>
+                <div className="login-password">
+                  <div>
+                    <label>Password</label>
+                  </div>
+                  <div>
+                    <input type="password" name="password" />
+                  </div>
+                </div>
+                <div className="login-btn">
+                  <button>Login</button>
+                </div>
+                <hr />
+              </div>
+            </form>
+          </div>
+          <div className="authgmail-container">
+            <div className="login-gmail">
+              <img src='/gmaillogo.png' />
+              <button>Login with Gmail</button>
+            </div>
+          </div>
+        </div>
       </GlassBox>
-    </div>
+    </div >
   )
 }
